0fae084556b76dcc31e91de6951e2c346d143215
Isaías Santana <isds.santana@gmail.com>
2019-10-22 10:51:28 -0300

    adjust masks

--- lib/masked_controller.dart
+++ lib/masked_controller.dart
@@ -2,34 +2,87 @@ library masked_controller;
 
 import 'package:flutter/material.dart';
 import 'package:masked_controller/mask.dart';
+import 'package:meta/meta.dart';
 
-class MaskedTextController extends TextEditingController {
-  Mask mask;
-  String _previewsText = '';
-  
-  MaskedTextController() {
+class MaskedController extends TextEditingController {
+  MaskedController({@required Mask mask}) {
+    assert(mask != null);
+
+    _mask = mask;
     addListener(_listener);
+  }
+
+  Mask _mask;
+  String _previewsText = '';
+
+  String get unmaskedText => _mask.removeMaskFrom(string: _previewsText) ?? '';
+
+  Mask get mask => _mask;
+
+  set mask(Mask mask) {
+    assert(mask != null);
+
+    _mask = mask;
+    update(text: text);
+  }
+
+  @override
+  set text(String newText) {
+    if (super.text != newText) {
+      int cursorPosition;
+
+      if (selection.baseOffset >= super.text.length) {
+        cursorPosition = (newText ?? '').length;
+      } else {
+        cursorPosition = selection.baseOffset;
+      }
+
+      final TextSelection textSelection =
+          TextSelection.collapsed(offset: cursorPosition);
+
+      value = value.copyWith(
+          text: newText, selection: textSelection, composing: TextRange.empty);
+    }
   }
 
   void _listener() {
     if (mask == null) return;
 
     final String currentText = text ?? '';
-    final String maskedText = mask.applyMaskTo(string: mask.removeMaskFrom(string: currentText));
+    final String maskedText =
+        mask.applyMaskTo(string: mask.removeMaskFrom(string: currentText));
 
     if (maskedText == null) {
-      print('is null, current text $currentText');
-      value = value.copyWith(text: _previewsText, selection: TextSelection.fromPosition(TextPosition(offset: _previewsText.length)));
-    } else {
-      if (_previewsText == maskedText) {
-        value = value.copyWith(text: maskedText, selection: TextSelection.fromPosition(TextPosition(offset: value.selection.end)));
-        return;
-      }
-
-      final int cursorPosition = (_previewsText.length - currentText.length) + selection.end;
-      _previewsText = maskedText;
-      value = value.copyWith(text: maskedText, selection: TextSelection.fromPosition(TextPosition(offset: cursorPosition)));
+      update(text: _previewsText);
+      _moveCursorToEnd();
+      return;
     }
+
+    update(text: maskedText ?? '');
+  }
+
+  void _moveCursorToEnd() {
+    final String text = this.text ?? '';
+    _moveCursorTo(position: text.length);
+  }
+
+  void _moveCursorTo({@required int position}) {
+    final TextPosition textPosition = TextPosition(offset: position);
+    final TextSelection textSelection =
+        TextSelection.fromPosition(textPosition);
+    value = value.copyWith(text: text, selection: textSelection);
+  }
+
+  void update({@required String text}) {
+    if (mask == null) return;
+
+    if (text != null) {
+      this.text = mask.applyMaskTo(string: mask.removeMaskFrom(string: text));
+    } else {
+      this.text = _previewsText;
+    }
+
+    _previewsText = text;
   }
 
   @override
